e3bf1aacad4609be01509f484571fbc2d81c89d7
Andres <frandres333@gmail.com>
2016-10-22 16:27:11 -0500

    error

--- db/migrate/20161021170101_create_teams.rb
+++ db/migrate/20161021170101_create_teams.rb
@@ -4,13 +4,13 @@ class CreateTeams < ActiveRecord::Migration
       t.string :name
       t.string :logo
       t.string :semester, limit: 2
-      t.references :perfomance, index: true
+      t.references :performance, index: true
       t.references :key, index: true
       t.references :phase, index: true
 
       t.timestamps null: false
     end
-    add_foreign_key :teams, :perfomances
+    add_foreign_key :teams, :performances
     add_foreign_key :teams, :keys
     add_foreign_key :teams, :phases
   end
