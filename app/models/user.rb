936dc7f6cc428515bca60341dc325549e8657693
Andres <frandres333@gmail.com>
2016-10-23 05:07:26 -0500

    casi completo

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,8 @@ class User < ActiveRecord::Base
   def self.from_omniauth(auth)
   	where(provider: auth["provider"], uid: auth["uid"]).first_or_create do |user|
   		if auth[:info]
-  			user.email = if auth[:info][:email].nil? then "#{auth[:info][:name]}@gmail.com" else auth[:info][:email] end
+  			user.email = if auth[:info][:email].nil? || auth[:info][:email].empty? then "#{auth[:info][:name]}@gmail.com" else auth[:info][:email] end
+        user.username = "#{auth[:info][:first_name]}#{rand(1000..5000)}"
         user.first_name = auth[:info][:first_name]
         user.last_name = auth[:info][:last_name]
   			user.name = auth[:info][:name]
